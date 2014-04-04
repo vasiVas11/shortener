40650117cef40c9ddc9208d7c87dbd1b4ae907b1
Vasile Pascu <vasilepascu@P5084.(none)>
2014-04-04 12:26:57 +0300

    init redis

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ENV["REDISTOGO_URL"] = 'redis://redistogo:58868c345d4b3d15d08fcd816b55afa4@grideye.redistogo.com:10332/'
-
 # Initialize the rails application
 Shortener::Application.initialize!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Shortener::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  ENV["REDISTOGO_URL"] = 'redis://redistogo:58868c345d4b3d15d08fcd816b55afa4@grideye.redistogo.com:10332/'
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
