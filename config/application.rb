8aca071774feb40923a58a56ae4c38b7d7ab6d70
Vasile Pascu <vasilepascu@P5084.(none)>
2014-04-04 14:45:14 +0300

    init redis

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Shortener
 
     config.autoload_paths += %W(#{config.root}/app/jobs )
 
+    config.assets.initialize_on_precompile=false
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Shortener::Application.routes.draw do
 
   root to: 'shorters#new'
 
-  resources :shorters
+  resources :shortersshorters
 
   match "/:identifier" => "shorters#redirect", as: "short_link"
 
