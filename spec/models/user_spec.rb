750b86dc8424f8579a03946c76c0b7a0d54cebcd
Vasile Pascu <vasilepascu@P5084.(none)>
2014-04-09 11:58:54 +0300

    test user class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "can be instantiated" do
+    User.new.should be_an_instance_of(User)
+  end
+
+  it "can create user" do
+    user = User.new(:email => "test_create_2@mailinator.com", :password => "blabla12")
+    expect { user.save }.to change(User, :count)
+  end
+
+  it "should not create user without email" do
+
+    user = User.new(:password => "blabla")
+    expect { user.save }.should_not change(User, :count)
+
+  end
+
+  it "should not create user with short password" do
+
+    user = User.new(:email => "test_create_23@mailinator.com", :password => "a12")
+    expect { user.save }.should_not change(User, :count)
+
+  end
+
+  it "should not create user with if email already exist" do
+
+    user = User.new(:email => "test_create_2@mailinator.com", :password => "blabla12")
+    user2 = User.new(:email => "test_create_2@mailinator.com", :password => "blabla12")
+    user.save
+    expect { user2.save }.should_not change(User, :count)
+
+  end
+
 end
